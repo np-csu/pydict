fd4ae9d385e1fc387265ea0e86b36143bacf453c
nipan <nipan1988@gmail.com>
2015-03-01 17:23:03 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+version = '0.1'
+
+setup(  name='pydict',
+        version=version,
+        description='An En-Cn Dictionary for Terminal',
+        author='nipan',
+        author_email='nipan1988@gmail.com',
+        url='https://github.com/np-csu/pydict.git',
+        install_requires=['termcolor']
+        )
+        
